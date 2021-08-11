ab91d2d288e790cae123afda7d6b8fb1e00ea581
Daniel Fernández <xsteguin@gmail.com>
2021-08-11 00:07:35 -0500

    Solved request failure. Just used sleep function.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 
 def download(number):
     pago = f"https://sibecas.uaemex.mx/registro/documentos/{number}/2021B/doc1.pdf"
@@ -13,11 +14,13 @@ def main(numbers):
         request_pago = requests.get(pago, allow_redirects=True)
         request_domicilio = requests.get(domicilio, allow_redirects=True)
         if request_pago.status_code != 404:
+            time.sleep(2)
             open('pago.pdf', 'wb').write(request_pago.content)
             open('domicilio.pdf', 'wb').write(request_domicilio.content)
             os.mkdir(number)
             os.rename("./pago.pdf", f"./{number}/pago.pdf")
             os.rename("./domicilio.pdf", f"./{number}/domicilio.pdf")
+        
             
 if __name__ == "__main__":
     numbers = [i for i in range(1310250,1410250)]
